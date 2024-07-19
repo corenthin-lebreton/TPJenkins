3ebffb52f7da67f17aa695e0229b63ee53272d00
root <root@DevSecOps.local>
2024-07-19 15:37:50 +0000

    Test Pipeline 6

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-#test modification 2
+#test modification 3
 $m = new mysqli("localhost", "user", "password", "db01");
 $m->query("SELECT * FROM users WHERE username='admin' AND 
 password='".$_REQUEST["pwd"]."'");
