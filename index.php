738793122446783f9880975469668cca452109e8
root <root@DevSecOps.local>
2024-07-19 15:29:56 +0000

    test Jenkins*

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-#test modification
+#test modification 2
 $m = new mysqli("localhost", "user", "password", "db01");
 $m->query("SELECT * FROM users WHERE username='admin' AND 
 password='".$_REQUEST["pwd"]."'");
