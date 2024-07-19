e239f4b4832f280f5228b21a73e077953cb6c927
root <root@DevSecOps.local>
2024-07-19 15:48:40 +0000

    zrgfrzefg

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-#test modification 35
+#test modification 340
 $m = new mysqli("localhost", "user", "password", "db01");
 $m->query("SELECT * FROM users WHERE username='admin' AND 
 password='".$_REQUEST["pwd"]."'");
